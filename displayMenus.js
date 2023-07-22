cf3e915617db37911ef3cf81993ae546b4aab175
Brandon Himpfen <brandonhimpfen@gmail.com>
2023-07-22 00:26:48 -0400

    Update displayMenus.js

--- displayMenus.js
+++ displayMenus.js
@@ -14,7 +14,7 @@ function displayContentsByIds(contentsMap) {
 // Defined menus
 const contentsMap = {
   'projects-menu': '<div class="submenu"><a href="/projects/">Projects</a> / <a href="/themes/">Themes</a> / <a href="/code/">Code</a> / <a href="/pages/">Pages</a> / <a href="/sites/">Sites</a> / <a href="/websites/">Websites</a></div>',
-  'themes-menu': '<div class="pills"><a href="/themes/" class="pills-link">Themes</a> <a href="/wordpress-themes/" class="pills-link">WordPress Themes</a> <a href="/ghost-themes/" class="pills-link">Ghost Themes</a> <a href="/jekyll-themes/" class="pills-link">Jekyll Themes</a> <a href="/bootstrap-themes/" class="pills-link">Bootstrap Themes</a> <a href="/starter-themes/" class="pills-link">Starter Themes</a></div>',
+  'themes-menu': '<div class="pills"><a href="/wordpress-themes/" class="pills-link">WordPress Themes</a> <a href="/ghost-themes/" class="pills-link">Ghost Themes</a> <a href="/jekyll-themes/" class="pills-link">Jekyll Themes</a> <a href="/bootstrap-themes/" class="pills-link">Bootstrap Themes</a> <a href="/starter-themes/" class="pills-link">Starters</a></div>',
 };
 
 // Display the contents based on the provided map
